7cc525b797f732d147073b9467b050c929f3c38a
Boyet120803 <b.dedal@mlgcl.edu.ph>
2025-04-06 18:10:21 +0800

    first commit

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+use illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return response()->json([
+            'users' => User::all(),
+        ]);
+    }
+
+        public function login(Request $request)
+        {
+            if (Auth::attempt([
+                'email' => $request->email,
+                'password' => $request->password,
+            ])) {
+                $user = Auth::user();
+                $token = $user->createToken('mytoken')->plainTextToken;
+        
+                return response()->json([
+                    'message' => 'Login successful',
+                    'token' => $token,
+                ]);
+            } else {
+                return response()->json([
+                    'message' => 'Invalid credentials',
+                ], 401);
+            }
+        }
+
+        public function store(Request $request){
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:8|confirmed',
+            ]);
+        
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+            ]);
+        
+            return response()->json([
+                'message' => 'User created successfully',
+                'user' => $user,
+            ]);
+        }
+    
+    }
+
